f572a6b0b8825cc1e14ab3e84523cb07deab5257
SDL <88381450+SDL-START@users.noreply.github.com>
2024-03-14 11:07:37 +0700

    LaoPT

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mac/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mac/LAOSPT/covid-insurance-app-development"
+export "FLUTTER_ROOT=E:\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\SDL\Desktop\112233\New folder\original-app-laopt"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.7"
